0b77caf52d5246653aac3d337529258d9d10701d
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2022-09-08 15:17:29 +1000

    test windows check error using cluster (again)
    access get_sample() by :: in case it's not accessing itself properly when the package is not attached (when being run on workers on cluster)

--- R/get_sample.R
+++ R/get_sample.R
@@ -32,7 +32,7 @@ get_sample <- function(auc, n_samples, prevalence, min_events = 0) {
   # if by chance all samples are either positive or negative, repeat the sampling
   # almost all the cutpoint selection methods will fail if there's only 1 class.
   if (n_pos == 0 | n_neg == 0) {
-    return(get_sample(auc, n_samples, prevalence, min_events))
+    return(predictNMB::get_sample(auc, n_samples, prevalence, min_events))
   }
 
   x <- c(stats::rnorm(n_neg, mean = 0), stats::rnorm(n_pos, mean = d))
